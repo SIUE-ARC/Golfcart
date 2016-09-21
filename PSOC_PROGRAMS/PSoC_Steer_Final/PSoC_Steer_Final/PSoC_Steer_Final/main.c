45613225e574cfa3becf3c650f15b868eba9f3a3
Zach Anderson <zach.inbox@gmail.com>
2016-09-21 13:39:14 -0500

    Refactored steer PSoC motor controller handling

--- PSOC_PROGRAMS/PSoC_Steer_Final/PSoC_Steer_Final/PSoC_Steer_Final/main.c
+++ PSOC_PROGRAMS/PSoC_Steer_Final/PSoC_Steer_Final/PSoC_Steer_Final/main.c
@@ -8,53 +8,93 @@
 #include <string.h>
 #include <stdlib.h>	
 
-//#define ON "on"
-//#define OFF "off"
-
 #pragma interrupt_handler PSoC_GPIO_ISR_C
-#define STEER_POT_CENTER 0x204
-#define ENCODER_LEFT_BOUND -2300
-#define ENCODER_RIGHT_BOUND 2300
+
 #define POT_LEFT_BOUND 0x00FF
+#define STEER_POT_CENTER 0x204
 #define POT_RIGHT_BOUND 0x02EF
 
-int val;
-BOOL encoderFlag = FALSE;
-int glblCount = 0;
-int reqCount = 0;
-unsigned int steerPotvalue = 0;
-unsigned int brakePotvalue = 0;
-BYTE prevPrt;
-BYTE curPrt;
-BYTE turning = 0;
-BYTE manTurn = 0;
-BYTE useBrake = 0;
-BYTE* cancelComm;
-char on[] = "on";
-char off[] = "off";
-int heartbeat = 0;
-//char* data;
+#define ENCODER_LEFT_BOUND -2300
+#define ENCODER_RIGHT_BOUND 2300
+
+#define BRAKE_MAX_POS 900
+#define BRAKE_MIN_POS 100
+
+#define STOP 0
+
+#define BAUD_BYTE 0xAA
+
+#define STEER_CTL 128
+#define STEER_SPEED 80
+#define LEFT 0
+#define RIGHT 1
+
+#define BRAKE_CTL 130
+#define BRAKE_SPEED 120
+#define RELEASE 0
+#define APPLY 1
+
+
+/******** PROTOTYPES ********/
+
+// Get the ADC values of the appropriate sensor. Values will be cached until cacheValid is reset
+unsigned int getBrakePosition(void);
+unsigned int getSteerPotPosition(void);
+
+// Tells the specified motor controller to run at the specfied speed. The
+// controller will continue to run until it is explicity stopped. setControllerSpeed
+// will cache the last parameters sent to each motor and only update the motor
+// controller if the values have changed.
+// Speed must be in the range [0, 127]
+// dir is either a 0 or 1
+void setControllerSpeed(BYTE addr, BYTE speed, BYTE dir);
+
+// Set the current turn and brake setpoints. Both are clamped between the appropriate defines
+void turn(int count);
+void brake(int pVal);
+
+// Stops all motors. This does not mean the brake will be applied.
+void stop(void);
+
+// Update the motor controller speeds based on the current setpoints
+void updateBrakeCtl(void);
+void updateTurnCtl(void);
+
+// Realign the steer encoder so that 0 counts occurs when the pot is centered
+void calibrateSteering(void);
 
 int command_lookup(BYTE cmd);
-void countEncoder(void );
-int turnToCount(int count);
-void sendSTOP(void );
-void applyBrake(int pVal );
-void releaseBrake(void );
-void turn(BYTE direction);
-unsigned int getActuatorPosition(void );
-unsigned int getSteerPotPosition(void );
-void resetPotShaft(void );
 
+int min(int, int);
+int max(int, int);
+int min(int a, int b) { return a < b ? a : b; }
+int max(int a, int b) { return a > b ? a : b; }
+
+
+/******** GLOBALS ********/
+
+// Encoder state variables
+BYTE prevPrt;
+BYTE curPrt;
+
+// Are currently cached analog reads valid, or should they be reaquired
+BYTE cacheValid;
+
+// The current count of the quadrature encoder connected to the steering column
+int steerCount;
+
+// The current target positions for both motor controllers
+int brakeSetpoint;
+int steerSetpoint;
+
+
+/******** MAIN ********/
 
 void main(void)
 {
 	char* data;
-	int lastCount = 0;
-	char baudChar = 0xAA;
-	//unsigned int potValue = 0;
-	val = 0;
 	
+	unsigned int heartbeat = 0;
 	
 	M8C_EnableGInt ; 
 	M8C_EnableIntMask(INT_MSK0,INT_MSK0_GPIO); 	// Enable GPIO Interrupts (see m8c.h)
@@ -73,8 +113,6 @@ void main(void)
 	LCD_Position(0,0);
 	LCD_PrCString("Steering PSoC");
 	
-	
-	
 	UART_CPutString("Steer Program Start\r\n");
 	//TX8_PutChar(baudChar);
 	//DAC8_1_Start(DAC8_1_HIGHPOWER);
@@ -82,50 +120,44 @@ void main(void)
 		
 	while (TRUE)
 	{
+		cacheValid = FALSE;
+		
 		prevPrt = (PRT1DR & (OpEncA_MASK | OpEncB_MASK)); 
-		if(heartbeat%100 == 0)
+		
+		if(heartbeat % 500 == 0)
 		{
 			UART_PutCRLF();
 			UART_CPutString("ping");
 			UART_PutCRLF();
 		}
 		heartbeat++;
+		
+		// If the e-stop line is low
 		if (!(PRT1DR & ESTOP_MASK)){
 			UART_CPutString("ESTOP");
 			UART_PutCRLF();
-			if (useBrake)applyBrake(900);
-			sendSTOP();
+			
+			stop();
+			LCD_Position(0,0);
+			LCD_PrCString("E-STOP");
+			
 			while (!(PRT1DR & ESTOP_MASK));
-			if(useBrake)releaseBrake();
 			UART_CPutString("RESUME");
 			UART_PutCRLF();
 		}
-		LCD_Position(0,0);
-		LCD_PrHexInt(glblCount);
-		LCD_Position(1,0);
-		LCD_PrHexInt(getSteerPotPosition());
 		
-		// If we passed the target point since last time
-		if ((lastCount < reqCount && glblCount >= reqCount || lastCount > reqCount && glblCount <= reqCount) && turning) 
-		{
-			sendSTOP();
-		}
-		lastCount = glblCount;
-		
-		//In the future try to get protection for every method of turning with pot values
-		/*if (turning || manTurn)
-		{
-			potValue = getSteerPotPosition();
-			if ((potValue >= POT_RIGHT_BOUND) || (potValue <= POT_LEFT_BOUND)) sendSTOP();
-		}*/
+		updateBrakeCtl();
+		updateTurnCtl();
+
 		if(UART_bCmdCheck()) 
-		{                    // Wait for command    
+		{
+			// Wait for command    
 			if(data = UART_szGetParam()) 
 			{
 				//UART_PutString(data);
 				command_lookup(*data);
 			}   
-		UART_CmdReset();  // Reset command buffer     
+			UART_CmdReset();  // Reset command buffer     
 		}
 	}
 }
@@ -135,133 +167,238 @@ int command_lookup(BYTE cmd)
 {
 	BYTE* data;
 	BYTE* TX;
-	BYTE addr = 128;
-	BYTE dir = 0;
-	BYTE val = 0;
-	BYTE checksum = 0;
 	BYTE baud = 0xAA;
 	int count = 0;
-	cancelComm = 0;
 	switch (cmd)
 	{
-		//turn to a specific count
+		// Turn to a specific count
 		case 'T':
 		case 't':
 			if (data = UART_szGetParam())
-			{
-				count = atoi(data);
-				if ((count < ENCODER_LEFT_BOUND) || (count > ENCODER_RIGHT_BOUND))
-				{
-					UART_CPutString("Count outside of bounds.\r\n");
-					break;
-				}
-				else
-				{
-					UART_CPutString("Turning to >");
-					UART_PutString(data);
-					UART_CPutString(" encoder ticks<\r\n");
-					return turnToCount(count);
-				}
-			}
-			else
-			{
-				UART_CPutString("No value given!\r\n");
-			}
+				turn(atoi(data));
+			
+			#ifdef VERBOSE
+				else UART_CPutString("No value given!\r\n");
+			#endif
 			break;
-		//send steer count
+						
+		// Apply the brake to the specified value [100, 900]
+		case 'h':
+		case 'H':
+			if (data = UART_szGetParam())
+				brake(atoi(data));
+			
+			#ifdef VERBOSE
+				else UART_CPutString("No brake value given!!!\r\n");
+			#endif
+			break;
+			
+		// Request the steer encoder count
 		case 'S':
 		case 's':
-			//UART_CPutString("Sending steer encoder count\r\n");
-			UART_PutString(itoa(TX,glblCount,10));
+			UART_PutSHexInt(steerCount);
 			UART_PutCRLF();
-			UART_CmdReset();
 			break;
-		//manual turning right
+			
+		// Manual turning right
 		case 'E':
 		case 'e':
-			UART_CPutString("Turning Right\r\n");
-			turn(1);
-			UART_CmdReset();
+			#ifdef VERBOSE
+				UART_CPutString("Turning Right\r\n");
+			#endif
+			
+			setControllerSpeed(STEER_CTL, STEER_SPEED, RIGHT);
 			break;
-		//manual turning left
+			
+		// Manual turning left
 		case 'Q':
 		case 'q':
-			UART_CPutString("Turning Left\r\n");
-			turn(0);
-			UART_CmdReset();
+			#ifdef VERBOSE
+				UART_CPutString("Turning Left\r\n");
+			#endif
+			
+			setControllerSpeed(STEER_CTL, STEER_SPEED, LEFT);
 			break;
+			
 		//manual stopping
 		case 'L':
 		case 'l':
-			UART_CPutString("Sending a manual STOP\r\n");
-			sendSTOP();
-			UART_CmdReset();
+			stop();
 			break;
-		//toggles the useBrake flag for estop purposes
-		case 'U':
-		case 'u':
-			if (useBrake) useBrake = 0;
-			else useBrake = 1;
-			break;
+			
 		//sending the baud character
 		case 'I':
 		case 'i':
 			UART_PutChar('S');
-			TX8_PutChar(baud);		
-			//resetPotShaft();
-			//UART_CPutString("Shaft Reset\r\n");
-			UART_CmdReset();
+			TX8_PutChar((CHAR)BAUD_BYTE);		
 			break;
+			
 		//reset the position of the wheels to '0'
 		case 'R':
 		case 'r':
-			resetPotShaft();
+			calibrateSteering();
 			UART_CPutString("Shaft Reset\r\n");
-			UART_CmdReset();
 			break;
-		//Putting the brake on or off
-		case 'h':
-		case 'H':
-			if (data = UART_szGetParam())
-			{
-				count = atoi(data);
-				//brake on
-				//if (strcmp(data,on)==0){
-				//	UART_CPutString("Brake ON\r\n");
-				sendSTOP();
-				applyBrake(count);
-				UART_CmdReset();
-				//brake off
-				//} else if (strcmp(data,off)==0){
-				//	UART_CPutString("Brake OFF\r\n");
-				//	sendSTOP();
-				//	releaseBrake();
-				//	UART_CmdReset();
-			}
-			else UART_CPutString("No brake value given!!!\r\n");
-			break;
+
+			
+		// Fully release the brake
 		case 'J':
 		case 'j':
-			sendSTOP();
-			releaseBrake();
+			brake(100);
 			break;
+			
+		// Request the value of the steer pot
 		case 'P':
 		case 'p':
-			steerPotvalue = getSteerPotPosition();
 			UART_CPutString("Steer pot is at >");
-			UART_PutSHexInt(steerPotvalue);
+			UART_PutSHexInt(getSteerPotPosition());
 			UART_CPutString(" counts<\r\n");
 			break;
+			
 		//Invalid command
 		default:
 			UART_CPutString("Invalid Command: >");
 			UART_PutChar(cmd);
 			UART_CPutString("<\n\r");
-			UART_CmdReset();
 			break;
 	}
+	UART_CmdReset();
 	return 0;
 }
+
+void setControllerSpeed(BYTE addr, BYTE speed, BYTE dir) {
+	static BYTE lastValue[2];
+	
+	if (lastValue[addr == BRAKE_CTL ? 1 : 0] != dir << 7 | speed) {
+		BYTE TX[4];
+		TX[0] = addr;
+		TX[1] = dir;
+		TX[2] = speed;
+		TX[3] = (addr + dir + speed) & 0x7F;
+		TX8_Write(TX,4);
+		
+		lastValue[addr == BRAKE_CTL ? 1 : 0] = dir << 7 | speed;
+	}
+}
+
+void stop(void) {
+	setControllerSpeed(STEER_CTL, STOP, STOP);
+	setControllerSpeed(BRAKE_CTL, STOP, STOP);
+	
+	#ifdef VERBOSE
+		UART_CPutString("Stopping all motors!!");
+		UART_PutCRLF();
+	#endif
+}
+
+void updateBrakeCtl(void) {
+	unsigned int brakePosition = getBrakePosition();
+	static int error;
+	
+	error = brakeSetpoint - brakePosition;
+	
+	if (error < -20)
+		setControllerSpeed(BRAKE_CTL, BRAKE_SPEED, APPLY);
+	else if (error > 20)
+		setControllerSpeed(BRAKE_CTL, BRAKE_SPEED, RELEASE);
+	else
+		setControllerSpeed(BRAKE_CTL, STOP, STOP);
+}
+
+void updateTurnCtl(void) {
+	int error = steerSetpoint - steerCount;
+	
+	if (error < -20)
+		setControllerSpeed(STEER_CTL, STEER_SPEED, RIGHT);
+	else if (error > 20)
+		setControllerSpeed(STEER_CTL, STEER_SPEED, LEFT);
+	else
+		setControllerSpeed(STEER_CTL, STOP, STOP);
+}
+
+unsigned int getBrakePosition(void)
+{
+	static unsigned int brakePot;
+	
+	if (!cacheValid) {
+		DUALADC_GetSamples(2);
+		
+		// Wait for data to be ready
+		while(DUALADC_fIsDataAvailable() == 0);
+			
+		// Get Data and clear flag
+		brakePot = DUALADC_iGetData2ClearFlag();
+	}
+	
+	return brakePot;
+}
+
+unsigned int getSteerPotPosition(void)
+{
+	static unsigned int steerPot;
+	
+	if (!cacheValid) {
+		DUALADC_GetSamples(2);
+		
+		// Wait for data to be ready
+		while(DUALADC_fIsDataAvailable() == 0);
+			
+		// Get Data and clear flag
+		steerPot = DUALADC_iGetData1ClearFlag();
+	}
+	
+	return steerPot;
+}
+
+void turn(int count)
+{
+	count = max(ENCODER_LEFT_BOUND, count);
+	count = min(ENCODER_RIGHT_BOUND, count);
+	
+	steerSetpoint = count;
+	
+	#ifdef VERBOSE
+		UART_CPutString("Steer setpoint: ");
+		UART_PutSHexInt(count);
+		UART_PutCRLF();
+	#endif
+}
+
+void brake(int pVal)
+{
+	pVal = max(BRAKE_MIN_POS, pVal);
+	pVal = min(BRAKE_MAX_POS, pVal);
+	
+	brakeSetpoint = pVal;
+	
+	#ifdef VERBOSE
+		UART_CPutString("Brake setpoint: ");
+		UART_PutSHexInt(pVal);
+		UART_PutCRLF();
+	#endif 
+}
+
+void calibrateSteering(void)
+{
+	BYTE dir = 0;
+	
+	unsigned int steerPotvalue = getSteerPotPosition();
+	
+	if (steerPotvalue < STEER_POT_CENTER) dir = RIGHT;
+	else if (steerPotvalue > STEER_POT_CENTER) dir = LEFT;
+	
+	setControllerSpeed(STEER_CTL, STEER_SPEED, dir);
+	
+	while (abs(steerPotvalue - STEER_POT_CENTER) > 30)
+	{
+		steerPotvalue = getSteerPotPosition();
+	}
+	
+	setControllerSpeed(STEER_CTL, STOP, STOP);
+	steerCount = 0;
+}
+
+/******** INTERRUPTS ********/
 
 //A ___|-----|_____|-----|____
 //B   ____|-----|_____|-----|____
@@ -275,7 +412,7 @@ void PSoC_GPIO_ISR_C(void)
 												// increment by turning clockwise and hitting a rising edge on A
 	{
 		// Increasing the count when clockwise turn interrupt occurred
-		glblCount++;
+		steerCount++;
 	}
 	else if ((prevPrt == 0x00) && (curPrt == 0x20))	// If prevPort is 0x00 and then after the interrupt curPrt is
 													// 0x20 then B is high and A is low which means you wanted to
@@ -283,240 +420,6 @@ void PSoC_GPIO_ISR_C(void)
 													// a rising edge on B
 	{
 		// Decreasing the count when the counterclockwise interrupt occurred
-		glblCount--;
+		steerCount--;
 	}
 }
-
-int turnToCount(int count)
-{
-	BYTE* TX;
-	BYTE addr = 128;
-	BYTE dir = 0;
-	BYTE val = 88;
-	BYTE checksum = 0;
-	int i = 0;
-	
-	reqCount = count;
-	
-	UART_CmdReset();
-//	if(turning){
-//		//for (i = 0; i < 56000; i++);
-//		sendSTOP();
-//		for (i = 0; i < 10000; i++);
-//	}
-	//UART_CPutString("turning left to>");
-	//UART_PutSHexInt(count);
-	//UART_CPutString("<\r\n");
-	//LCD_Control(LCD_DISP_CLEAR_HOME);
-	
-	//LCD_Position(0,0);
-	//LCD_PrCString("Going Left!!");
-	//dir = 0;
-	turning = 1;
-	if (count >= glblCount)dir = 1;
-	
-	//else if (((count == 0)&&(glblCount > 0))||(count < glblCount))dir = 0;
-	else if (count < glblCount)dir = 0;
-	
-	checksum = addr + dir + val;
-	checksum = checksum & 0x7F;
-	
-	TX[0] = addr;
-	TX[1] = dir;
-	TX[2] = val;
-	TX[3] = checksum;
-	TX8_Write(TX,4);
-
-	return count;
-}
-
-void sendSTOP(void ){
-	BYTE* TX;
-	BYTE addr = 128;
-	BYTE dir = 0;
-	BYTE val = 0;
-	BYTE checksum = 0;
-	
-	UART_CmdReset();
-	//val = *data;
-	//checksum = addr + dir + val;
-	//checksum = checksum & 0x7F;
-	//checksum &= (addr | dir | val);
-	manTurn = 0;
-	turning = 0;
-	TX[0] = addr;
-	TX[1] = dir;
-	TX[2] = val;
-	TX[3] = checksum;
-	TX8_Write(TX,4);
-	UART_CPutString("Stopping!!\r\n");
-}
-
-void applyBrake(int pVal)
-{
-	BYTE* TX;
-	BYTE addr = 130;
-	BYTE dir = 1;
-	BYTE val = 120;
-	BYTE checksum = 0;
-	if (pVal < 100 || pVal > 900)
-	{
-		UART_CPutString("Only enter a brake value between 100-900!!!\r\n");
-		return ;
-	}
-	
-	dir = (pVal >= brakePotvalue) ? 1:0;
-	
-	UART_CmdReset();
-	checksum = addr + dir + val;
-	checksum = checksum & 0x7F;
-	TX[0] = addr;
-	TX[1] = dir;
-	TX[2] = val;
-	TX[3] = checksum;
-	TX8_Write(TX,4);
-	
-	UART_CPutString("Braking!!\r\n");
-	while ((dir && (getActuatorPosition() < pVal)) || (!dir && (getActuatorPosition() > pVal)));
-	TX[2] = 0;
-	checksum = addr + dir;
-	checksum = checksum & 0x7F;
-	TX[3] = checksum;
-	TX8_Write(TX,4);
-	 // 96
-}
-
-void releaseBrake(void )
-{
-	BYTE* TX;
-	BYTE addr = 130;
-	BYTE dir = 0;
-	BYTE val = 120;
-	BYTE checksum = 0;
-	
-	UART_CmdReset();
-	checksum = addr + dir + val;
-	checksum = checksum & 0x7F;
-	TX[0] = addr;
-	TX[1] = dir;
-	TX[2] = val;
-	TX[3] = checksum;
-	TX8_Write(TX,4);
-	
-	UART_CPutString("Unbraking!!\r\n");
-	while (getActuatorPosition() > 100);
-	TX[2] = 0;
-	checksum = addr + dir;
-	checksum = checksum & 0x7F;
-	TX[3] = checksum;
-	TX8_Write(TX,4);
-	
-	UART_CPutString("FREEEEEDOM!!\r\n");
-}
-
-unsigned int getActuatorPosition(void )
-{
-	DUALADC_GetSamples(2);
-	// Wait for data to be ready
-	while(DUALADC_fIsDataAvailable ()==0);
-		
-	// Get Data and clear flag
-	brakePotvalue = DUALADC_iGetData2ClearFlag();
-	return brakePotvalue;
-}
-
-unsigned int getSteerPotPosition(void )
-{
-	unsigned int result = 0;
-	DUALADC_GetSamples(2);
-	// Wait for data to be ready
-	while(DUALADC_fIsDataAvailable ()==0);
-		
-	// Get Data and clear flag
-	result=DUALADC_iGetData1ClearFlag();
-	return result;
-}
-
-void turn(BYTE direction)
-{
-	BYTE* TX;
-	BYTE addr = 128;
-	BYTE dir;
-	BYTE val = 80;
-	BYTE checksum = 0;
-	int i  = 0;
-	
-	UART_CmdReset();
-	//val = *data;
-	if(manTurn){
-		//for (i = 0; i < 56000; i++);
-		sendSTOP();
-		for (i = 0; i < 10000; i++);
-	}
-	manTurn = 1;
-	dir = direction;
-	checksum = addr + dir + val;
-	checksum = checksum & 0x7F;
-	//checksum &= (addr | dir | val);
-	TX[0] = addr;
-	TX[1] = dir;
-	TX[2] = val;
-	TX[3] = checksum;
-	TX8_Write(TX,4);
-}
-
-void resetPotShaft(void)
-{
-	BYTE* TX;
-	BYTE addr = 128;
-	BYTE dir = 0;
-	BYTE val = 80;
-	BYTE checksum = 0;
-	int i = 0;
-	int avg = 0;
-	int tol = 16;
-	
-	UART_CmdReset();
-	//UART_CPutString("turning left to>");
-	//UART_PutSHexInt(count);
-	//UART_CPutString("<\r\n");
-	//LCD_Control(LCD_DISP_CLEAR_HOME);
-	
-	//LCD_Position(0,0);
-	//LCD_PrCString("Going Left!!");
-	//dir = 0;
-	steerPotvalue = getSteerPotPosition();
-	if (steerPotvalue < STEER_POT_CENTER)dir = 1;
-	
-	//else if (((count == 0)&&(glblCount > 0))||(count < glblCount))dir = 0;
-	else if (steerPotvalue > STEER_POT_CENTER)dir = 0;
-	
-	checksum = addr + dir + val;
-	checksum = checksum & 0x7F;
-	
-	TX[0] = addr;
-	TX[1] = dir;
-	TX[2] = val;
-	TX[3] = checksum;
-	TX8_Write(TX,4);
-	
-	while (steerPotvalue != STEER_POT_CENTER)
-	{
-		steerPotvalue = getSteerPotPosition();
-//		avg += steerPotvalue;
-//		i++;
-//		if (i == 5)
-//		{
-//			i = 0;
-//			avg = avg/5;
-//			if (avg >= (steerPotvalue + tol) || avg <= (steerPotvalue - tol))
-//			{
-//				sendSTOP();
-//				break;	
-//			}
-//		}
-	}
-	sendSTOP();
-	glblCount = 0;
-}
-	
